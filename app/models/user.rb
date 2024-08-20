6490a270a88f0a6d5083bbf62eff81af2c2f3573
Kathy Kolonics <kata.kolonics@gmail.com>
2024-08-20 15:20:14 +0200

    defined new/create actions for adding a new island

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :islands
+  has_many :bookings
+  has_many :received_bookings, through: :islands, source: :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
