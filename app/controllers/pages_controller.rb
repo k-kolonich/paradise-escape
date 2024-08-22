c8c586856b39db2cb78b89069a0c5aca991d40e1
Kathy Kolonics <kata.kolonics@gmail.com>
2024-08-22 16:50:33 +0200

    worked on what information is displayed for each booking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
     @user = current_user.id
     @islands = Island.where(user_id: @user)
     @user_bookings = Booking.where(user_id: @user)
+    @booked_islands = Island.joins(:bookings).where("bookings.user_id = ?", @user)
     @bookings = Booking.joins(:island).where("islands.user_id = ?", @user)
   end
 end
