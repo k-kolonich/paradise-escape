b1cbaedf5847454ad3381d67ab5bfbd515fd8d03
Aleksandra Stoliarova <stoliarova.alek@gmail.com>
2024-08-23 12:41:26 +0200

    Migration for images, added new images. Substitute with seed images in home page, dashboard, and show page. Avatar updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,7 @@ sunny_retreat = {
   description: "A beautiful sunny island with white sandy beaches and crystal-clear waters.",
   location: "Bahamas",
   price: 5000,
+  image: "https://images.pexels.com/photos/27050408/pexels-photo-27050408/free-photo-of-a-boat-is-sitting-on-the-beach-near-palm-trees.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 }
 
 tropical_paradise = {
@@ -24,6 +25,7 @@ tropical_paradise = {
   description: "Experience the ultimate tropical paradise with lush greenery and serene surroundings.",
   location: "Maldives",
   price: 8000,
+  image: "https://images.pexels.com/photos/1681148/pexels-photo-1681148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 }
 
 ocean_breeze = {
@@ -32,6 +34,7 @@ ocean_breeze = {
   description: "A peaceful escape surrounded by the ocean, perfect for relaxation.",
   location: "Fiji",
   price: 4500,
+  image: "https://images.pexels.com/photos/5707674/pexels-photo-5707674.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 }
 
 island_escape = {
@@ -40,6 +43,7 @@ island_escape = {
   description: "A secluded island with breathtaking views and luxury amenities.",
   location: "Seychelles",
   price: 9000,
+  image: "https://images.pexels.com/photos/2310835/pexels-photo-2310835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 }
 
 coconut_cove = {
@@ -48,6 +52,7 @@ coconut_cove = {
   description: "A cozy island retreat with palm trees and a private beach.",
   location: "Hawaii",
   price: 6500,
+  image: "https://images.pexels.com/photos/19183353/pexels-photo-19183353/free-photo-of-palm-trees-on-a-beach-seen-from-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 }
 
 sunset_sands = {
@@ -56,6 +61,7 @@ sunset_sands = {
   description: "A serene island escape with golden sands and breathtaking sunsets.",
   location: "Maldives",
   price: 8200,
+  image: "https://images.pexels.com/photos/1133540/pexels-photo-1133540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 }
 
 [sunny_retreat, tropical_paradise, ocean_breeze, island_escape, coconut_cove, sunset_sands].each do |attributes|
